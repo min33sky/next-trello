0103fcef6ae88cd09d787af2ac675ec001fac8a8
min33sky <min33sky@naver.com>
2023-06-04 17:05:36 +0900

    feat: column dnd finish

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,17 +6,21 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 export default function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
-
-  const [isMobileView, setIsMobileView] = useState(true);
-  const elementRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
+
+  /**
+   * @description 화면 크기에 따라 Column을 세로로 또는 가로로 렌더링한다.
+   */
+  const [isMobileView, setIsMobileView] = useState(true);
+  const elementRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const element = elementRef.current;
@@ -25,7 +29,7 @@ export default function Board() {
     const observer = new ResizeObserver(() => {
       const width = element.clientWidth;
       const height = element.clientHeight;
-      console.log('resize: ', width, height);
+
       if (width < 768) {
         setIsMobileView(true);
       } else {
@@ -40,8 +44,50 @@ export default function Board() {
     };
   }, []);
 
+  /**
+   * @description Column을 드래그앤드롭 했을 때, Board의 상태를 업데이트한다.
+   */
   const handleDragEnd = (result: DropResult) => {
-    console.log('handleDragEnd: ', result, 'TODO: update board');
+    const { type, destination, source } = result;
+
+    // 유저가 보드 밖으로 드래그를 했는지 체크
+    if (!destination) return;
+
+    // Column을 드래그앤드롭 했을 때
+    if (type === 'column') {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    } else if (type === 'card') {
+      // This step is need as the indexes are stored as numbers 0,1,2 etc. instead of id's with DND Library
+      console.log('DnD Result: ', result);
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const finishColIndex = columns[Number(destination.droppableId)];
+
+      console.log('columns: ', columns);
+      console.log('startColIndex: ', startColIndex);
+      console.log('finishColIndex: ', finishColIndex);
+
+      const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+      };
+
+      const finishCol: Column = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+      };
+
+      console.log('startCol: ', startCol);
+      console.log('finishCol: ', finishCol);
+    }
   };
 
   return (
--- src/store/boardStore.ts
+++ src/store/boardStore.ts
@@ -1,9 +1,12 @@
+import { databases } from '@/libs/appwrite';
 import { getTodosGroupedByColumn } from '@/libs/getTodosGroupedByColumn';
 import { create } from 'zustand';
 
 interface BoardState {
   board: Board;
-  getBoard: () => void;
+  getBoard: () => void; // DB에서 Board 정보를 가져와서 board state를 업데이트
+  setBoardState: (board: Board) => void; // Client에서 Board 정보를 업데이트
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void; // DB에서 Todo 정보를 업데이트
 }
 
 const useBoardStore = create<BoardState>((set) => ({
@@ -13,6 +16,18 @@ const useBoardStore = create<BoardState>((set) => ({
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
     set({ board });
+  },
+  setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+      process.env.NEXT_PUBLIC_APPWRITE_TODOS_COLLECTION_ID,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      },
+    );
   },
 }));
 
