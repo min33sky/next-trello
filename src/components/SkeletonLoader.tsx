3d3944546e918eeed7caaf6fab7beff6f7cdc180
min33sky <min33sky@naver.com>
2023-06-07 14:13:35 +0900

    fix: skeleton style

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -1,8 +1,8 @@
 export default function SkeletonLoader() {
   return (
     <div className="flex animate-pulse space-x-4 px-2">
-      <div className="flex-1 rounded-md bg-slate-300" />
-      <div className="h-12 w-12 rounded-full bg-slate-300 " />
+      <div className="flex-1 rounded-md bg-gradient-to-br from-slate-300 to-slate-100" />
+      <div className="h-12 w-12 rounded-full bg-gradient-to-tr from-slate-300 to-slate-100 " />
     </div>
   );
 }
